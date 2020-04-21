a48b78dc7a60cd2f78827b18739d9fcb7bb9d389
Mohammad-Ashraful <mohammadashrafulislam11@gmail.com>
2020-04-21 18:17:02 +0600

    first commit

---
+++ src/fakeData/user.js
@@ -0,0 +1,124 @@
+var user = [
+    {
+        "id": 1,
+        "name": "Bradley Bland",
+        "occupation": "Web Developer",
+        "email": "bradley@gmail.com",
+        "salary": 1000,
+        "img": "https://randomuser.me/api/portraits/men/85.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Jane Priest",
+        "occupation": "Graphic Designer",
+        "email": "janePriest@gmail.com",
+        "salary": 800,
+        "img": "https://randomuser.me/api/portraits/women/85.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Candi Barker",
+        "occupation": "Web Designer",
+        "email": "cbarker@yahoo.com",
+        "salary": 700,
+        "img": "https://randomuser.me/api/portraits/men/37.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Mary Curran",
+        "occupation": "UI/UX Designer",
+        "email": "marycurran@yahoo.com",
+        "salary": 1100,
+        "img": "https://randomuser.me/api/portraits/women/44.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Norma Bjork",
+        "occupation": "Android Developer",
+        "email": "normabjork@hotmail.com",
+        "salary": 900,
+        "img": "https://randomuser.me/api/portraits/men/91.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Sandra White",
+        "occupation": "Web Developer",
+        "email": "sandrawhite@gmail.com",
+        "salary": 1000,
+        "img": "https://randomuser.me/api/portraits/women/81.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Alvin Vitela",
+        "occupation": "Article Writer",
+        "email": "alvinvtla@outlook.com",
+        "salary": 600,
+        "img": "https://randomuser.me/api/portraits/women/17.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Alma Harrison",
+        "occupation": "Graphic Designer",
+        "email": "almaharri@gmail.com",
+        "salary": 800,
+        "img": "https://randomuser.me/api/portraits/women/12.jpg"
+    },
+    {
+        "id": 9,
+        "name": "Joe Cox",
+        "occupation": "Web Developer",
+        "email": "joecox34@yahoo.com",
+        "salary": 1000,
+        "img": "https://randomuser.me/api/portraits/men/78.jpg"
+    },
+    {
+        "id": 10,
+        "name": "William Hensley",
+        "occupation": "Android Developer",
+        "email": "williamhley@gmail.com",
+        "salary": 900,
+        "img": "https://randomuser.me/api/portraits/men/24.jpg"
+    },
+    {
+        "id": 11,
+        "name": "Albert Rhodes",
+        "occupation": "Article Writer",
+        "email": "albertrhodes@yahoo.com",
+        "salary": 600,
+        "img": "https://randomuser.me/api/portraits/men/28.jpg"
+    },
+    {
+        "id": 12,
+        "name": "Samuel Arellano",
+        "occupation": "Sales Promotion",
+        "email": "samarellano@outlook.com",
+        "salary": 700,
+        "img": "https://randomuser.me/api/portraits/men/33.jpg"
+    },
+    {
+        "id": 13,
+        "name": "Nancy Tabb",
+        "occupation": "Wordpress Developer",
+        "email": "nancytabb@gmail.com",
+        "salary": 800,
+        "img": "https://randomuser.me/api/portraits/women/72.jpg"
+    },
+    {
+        "id": 14,
+        "name": "Paul Gibbons",
+        "occupation": "Java Programmer",
+        "email": "pgibbson@hotmail.com",
+        "salary": 1000,
+        "img": "https://randomuser.me/api/portraits/men/87.jpg"
+    },
+    {
+        "id": 15,
+        "name": "Joseph Williams",
+        "occupation": "Web Developer",
+        "email": "jwilliams@gmail.com",
+        "salary": 1000,
+        "img": "https://randomuser.me/api/portraits/men/9.jpg"
+    },
+];
+
+export default user;
